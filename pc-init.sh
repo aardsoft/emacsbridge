c68e921d0befed681ce32400a92921c2b9016435
Bernd Wachter <bernd-github@wachter.fi>
2020-06-12 14:19:42 +0300

    dev: Set qml path, load-path and load emacsbridge-rpc.el
    This should allow an easy start on a development pc, with running
    `pc-init.sh' as only requirement to get everything set up properly.

--- pc-init.sh
+++ pc-init.sh
@@ -5,3 +5,12 @@
 
 SERVER_SOCKET=`emacsclient -e '(concat server-socket-dir "/server")'|sed 's/"//g'`
 curl -v -d $SERVER_SOCKET --header "setting: server-socket" localhost:1616/settings
+
+echo
+echo "Checking if variables should be set"
+LISP_PATH=`pwd`/lisp
+QML_PATH=`pwd`/qml
+
+emacsclient -e "(progn(add-to-list 'load-path \"$LISP_PATH\")(message \"Configuring load-path\"))"
+emacsclient -e "(if (and (boundp 'emacsbridge-default-qml-path) emacsbridge-default-qml-path)(message \"emacsbridge-default-qml-path is set, not touching it\")(progn (setq emacsbridge-default-qml-path \"$QML_PATH\")(message \"setting emacsbridge-default-qml-path\")))"
+emacsclient -e "(require 'emacsbridge-rpc)"
