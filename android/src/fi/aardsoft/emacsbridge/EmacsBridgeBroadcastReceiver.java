ac2617247c2d9d00056004a0dbb5f10f65df7c5f
Bernd Wachter <bernd-git@wachter.fi>
2020-10-19 00:13:41 +0300

    Fix service start on Android boot

--- android/src/fi/aardsoft/emacsbridge/EmacsBridgeBroadcastReceiver.java
+++ android/src/fi/aardsoft/emacsbridge/EmacsBridgeBroadcastReceiver.java
@@ -1,5 +1,6 @@
 package fi.aardsoft.emacsbridge;
 
+import android.os.Build;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,7 +8,11 @@ import android.content.Intent;
 public class EmacsBridgeBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent startServiceIntent = new Intent(context, EmacsBridgeBroadcastReceiver.class);
-        context.startService(startServiceIntent);
+        Intent startServiceIntent = new Intent(context, EmacsBridgeService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+          context.startForegroundService(new Intent(context, EmacsBridgeService.class));
+        } else {
+          context.startService(startServiceIntent);
+        }
     }
 }
